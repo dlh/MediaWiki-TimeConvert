f7f4f7927a6d81496d2ee7aa7040cfa79dcbe38a
Daniel Lee Harple <dlh@harple.com>
2014-03-26 13:46:00 -0400

    Initial commit.

---
+++ TimeConvert.i18n.php
@@ -0,0 +1,12 @@
+<?php
+
+// Copyright (C) 2014 DLH
+// See LICENSE.txt for the MIT license.
+ 
+$magicWords = array();
+ 
+$magicWords["en"] = array(
+   "timeconvert" => array(0, "timeconvert"),
+);
+
+?>
