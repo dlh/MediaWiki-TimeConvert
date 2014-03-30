368fc29316d6ab881a76d3014499c7689063564a
Daniel Lee Harple <git@dlh.dev>
2014-03-30 12:17:12 -0400

    Localize error messages.

--- TimeConvert.i18n.php
+++ TimeConvert.i18n.php
@@ -4,9 +4,14 @@
 // See LICENSE.txt for the MIT license.
  
 $magicWords = array();
- 
 $magicWords["en"] = array(
    "timeconvert" => array(0, "timeconvert"),
+);
+
+$messages = array();
+$messages["en"] = array(
+    "timeconvert-notime" => "No time specified, using '$1'",
+    "timeconvert-nozone" => "No zone name specified, using '$1'"
 );
 
 ?>
