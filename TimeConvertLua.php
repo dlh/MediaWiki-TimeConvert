9403328eb4948b0d3bc575d9c7f3b720024670b5
Daniel Lee Harple <dlh@harple.com>
2015-04-07 10:15:42 -0400

    Minor whitespace style change to use spaces

--- TimeConvertLua.php
+++ TimeConvertLua.php
@@ -2,11 +2,11 @@
 
 class TimeConvertLua extends Scribunto_LuaLibraryBase
 {
-	public function register()
+    public function register()
     {
-		$lib = array("timeconvert" => array($this, "timeconvert"));
-		$this->getEngine()->registerInterface( __DIR__ . "/TimeConvert.lua", $lib);
-	}
+        $lib = array("timeconvert" => array($this, "timeconvert"));
+        $this->getEngine()->registerInterface( __DIR__ . "/TimeConvert.lua", $lib);
+    }
 
     public function timeconvert($time="", $zoneName="", $format="")
     {
